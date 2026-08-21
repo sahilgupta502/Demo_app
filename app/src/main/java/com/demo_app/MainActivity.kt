30dbf4d1c73f89fa3f465cd8245c4abd85cb2e2e
Sahil <sahilgupta502@gmail.com>
2026-08-21 18:56:26 +0530

    Changes in UI

--- app/src/main/java/com/demo_app/MainActivity.kt
+++ app/src/main/java/com/demo_app/MainActivity.kt
@@ -3,13 +3,19 @@ package com.demo_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
-
+            Column(){
+                Text(text = "Hello World", fontSize = 15.sp, color = Color.Black)
+            }
     }
 }
 
