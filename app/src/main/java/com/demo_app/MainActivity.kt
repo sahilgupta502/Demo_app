145b87281d4877cd14fad24426669c394cbf3a15
Sahil <sahilgupta502@gmail.com>
2026-08-27 22:23:46 +0530

    fssgfslllllg

--- app/src/main/java/com/demo_app/MainActivity.kt
+++ app/src/main/java/com/demo_app/MainActivity.kt
@@ -115,7 +115,7 @@ Toast.makeText(context,"Good Evening Sir", Toast.LENGTH_SHORT).show()
                     modifier = Modifier.padding(10.dp)
                         .background(color = Color.LightGray, shape = RoundedCornerShape(10.dp)).fillMaxWidth().height(200.dp),
                     state = rememberPagerState(
-                        initialPage = 1,
+                        initialPage = 2,
                         pageCount = {list.size}
                     )
                 ) {index->
@@ -132,7 +132,7 @@ Toast.makeText(context,"Good Evening Sir", Toast.LENGTH_SHORT).show()
                     list.map {
                         Box(
                             modifier = Modifier.padding(5.dp).size(20.dp).clip(CircleShape)
-                                .background(color = Color.LightGray)
+                                .background(color = Color.Green)
                         )
                     }
                 }
